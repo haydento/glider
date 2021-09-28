24c668e240a55f81143ee40f0b2547210a49f1c6
chrisholbrook <cholbrook@usgs.gov>
2021-09-28 00:21:31 -0400

    added get_instr_data() to make clean_vem_detections_geo

--- src/vem.R
+++ src/vem.R
@@ -189,3 +189,213 @@ infer_detection_locations <- function(dtc, pos){
                               ties = "ordered")$y)]
 }
 
+
+#' Add info about tags and receivers from instr log to clean_vem_detections
+#' 
+#' @param dtc cleaned detection data from glider with locations added
+#' 
+#' @param hst instrument history log (tag and reciever metadata)
+#' 
+#' @examples
+#' targets::tar_load("clean_vem_detections")
+#' dtc <- clean_vem_detections
+#' 
+#' targets::tar_load("instr_deploy_data")
+#' hst <- instr_deploy_data
+
+get_instr_data <- function(dtc, hst){
+  
+  hst_file <- hst[[1]]
+  if(length(hst) > 1) stop("Can only load one hst file. Need to expand.")
+  hst <- data.table::fread(hst_file)  
+  
+  #set all missing timestamps to now for convenience
+  if(!inherits(hst$timestamp_end_utc, "POSIXct")) {
+    hst[ , timestamp_end_utc := as.POSIXct(timestamp_end_utc)]
+    hst[is.na(timestamp_end_utc), timestamp_end_utc := Sys.time()]
+  }
+  
+  #deep copy
+  dtc <- data.table::as.data.table(dtc)
+  hst <- data.table::as.data.table(hst)
+  
+  # Rename all dtc cols to transmitter_, receiver_, glider_ col names
+  
+  # recievers
+  rec_col_names <- c("serial_no", "frequency", "noise_level_db",
+                              "channel", "source_file")
+  data.table::setnames(dtc, rec_col_names, paste0("receiver_", rec_col_names))
+                     
+
+  # transmitters
+  tag_col_names <- c("sensor_value_adc", "signal_level_db")
+  data.table::setnames(dtc, tag_col_names, paste0("transmitter_", 
+                                                  tag_col_names))    
+  
+  # combine code space and id to match hst
+  dtc[ , transmitter_instr_id := paste0(transmitter_code_space, "-",
+                                        transmitter_id),
+       by = c("transmitter_code_space", "transmitter_id")]
+  
+  # glider
+  gld_col_names <- c("lon_dd", "lat_dd", "m_depth")
+  data.table::setnames(dtc, gld_col_names, paste0("glider_", gld_col_names))    
+  
+  
+  # Split hst by instr type and add instr prefix to column names as above
+  
+  # receivers
+  hst_rec <- hst[instr == "receiver"]
+  
+  #  rename instr_id to serial_no to match dtc
+  data.table::setnames(hst_rec, "instr_id", "serial_no")
+  
+  #  add receiver_ prefix
+  data.table::setnames(hst_rec, names(hst_rec), 
+                       paste0("receiver_", names(hst_rec)))
+
+
+  # transmitters
+  hst_tag <- hst[instr == "tag"]
+
+  #  parse instr_id to serial_no to match dtc
+  data.table::setnames(hst_tag, names(hst_tag), 
+                       paste0("transmitter_", names(hst_tag)))  
+
+  
+  #--------------------------------------
+  # BIG JOINS
+  
+  # STEP 1. Non equi join hst_tag with dtc by transmitter id, datetime
+  
+  dtc2 <- dtc[hst_tag, on = list(datetime >= transmitter_timestamp_start_utc,
+                                 datetime <= transmitter_timestamp_end_utc,
+                                 transmitter_instr_id = transmitter_instr_id),
+                  list(datetime = x.datetime, 
+                       receiver_serial_no, 
+                       receiver_channel,
+                       receiver_frequency,
+                       receiver_source_file,
+                       receiver_noise_level_db,
+                       transmitter_run_id,
+                       transmitter_run,
+                       transmitter_instr,
+                       transmitter_mooring_type,
+                       transmitter_instr_model,
+                       transmitter_freq,
+                       transmitter_instr_id,
+                       transmitter_site,
+                       transmitter_latitude,
+                       transmitter_longitude,
+                       transmitter_water_depth,
+                       transmitter_instr_elevation_from_bottom,
+                       transmitter_instr_depth_from_top,
+                       transmitter_timestamp_start_utc,
+                       transmitter_timestamp_end_utc,
+                       transmitter_comment,
+                       transmitter_sensor_value_adc,
+                       transmitter_signal_level_db,
+                       glider_lon_dd,
+                       glider_lat_dd,
+                       glider_m_depth
+                       )]
+  
+
+  # STEP 2. Non equi join hst_rec with dtc by transmitter id, datetime
+  
+  dtc2 <- dtc2[hst_rec, 
+                         on = list(datetime >= receiver_timestamp_start_utc,
+                                   datetime <= receiver_timestamp_end_utc,
+                                   receiver_serial_no = receiver_serial_no),
+                  list(datetime = x.datetime, 
+                       receiver_run_id,
+                       receiver_run,
+                       receiver_site,
+                       receiver_instr,
+                       receiver_mooring_type,
+                       receiver_instr_model,
+                       receiver_serial_no, 
+                       receiver_channel,
+                       receiver_freq,
+                       receiver_frequency,
+                       receiver_latitude,
+                       receiver_longitude,
+                       receiver_water_depth,
+                       receiver_instr_elevation_from_bottom,
+                       receiver_instr_depth_from_top,
+                       receiver_timestamp_start_utc,
+                       receiver_timestamp_end_utc,
+                       receiver_comment,
+                       receiver_source_file,
+                       receiver_noise_level_db,
+                       transmitter_run_id,
+                       transmitter_run,
+                       transmitter_instr,
+                       transmitter_mooring_type,
+                       transmitter_instr_model,
+                       transmitter_freq,
+                       transmitter_instr_id,
+                       transmitter_site,
+                       transmitter_latitude,
+                       transmitter_longitude,
+                       transmitter_water_depth,
+                       transmitter_instr_elevation_from_bottom,
+                       transmitter_instr_depth_from_top,
+                       transmitter_timestamp_start_utc,
+                       transmitter_timestamp_end_utc,
+                       transmitter_comment,
+                       transmitter_sensor_value_adc,
+                       transmitter_signal_level_db,
+                       glider_lon_dd,
+                       glider_lat_dd,
+                       glider_m_depth
+                       )]  
+  
+  # STEP 3. Update receiver_ and transmitter_ lonlat with glider if mary_lou
+  
+  #!!! DANGEROUS SHORTCUT HERE FOR NOW !!!!!!!!!!!!!!
+  
+  # drop is.na(datetime)??? - is this an artifact of the non-equi join?
+  #   or evidence that something went wrong???
+  dtc2 <- dtc2[!is.na(datetime)]
+  
+  
+  #Update receiver records with relevant from glider
+  dtc2[receiver_site == "mary_lou" & receiver_mooring_type == "mobile",
+       `:=`(receiver_latitude = glider_lat_dd,
+            receiver_longitude = glider_lon_dd,
+            receiver_water_depth = NA, #!! calc from depth + altitude
+            receiver_instr_elevation_from_bottom = NA, # !! missing from dtc
+            receiver_instr_depth_from_top = glider_m_depth
+       )]
+ 
+  #Update transmitter records with relevant from glider
+  dtc2[transmitter_site == "mary_lou" & transmitter_mooring_type == "mobile",
+       `:=`(transmitter_latitude = glider_lat_dd,
+            transmitter_longitude = glider_lon_dd,
+            transmitter_water_depth = NA, #!! calc from depth + altitude
+            transmitter_instr_elevation_from_bottom = NA, # !! missing from dtc
+            transmitter_instr_depth_from_top = glider_m_depth
+       )] 
+  
+  
+  # ----------------------
+  
+  # Calculate distance between tag and receiver at each transmission
+  dtc2[ , `:=`(
+            rt_distance_meters = geosphere::distVincentyEllipsoid(
+                      p1 = cbind(receiver_longitude, receiver_latitude), 
+                      p2 = cbind(transmitter_longitude, transmitter_latitude)))]
+
+  # Calculate heading between glider and transmitter
+  dtc2[(receiver_longitude != transmitter_longitude) |  
+       (receiver_latitude != transmitter_latitude), 
+       `:=`(
+          rt_heading_degrees = geosphere::bearing(
+            p1 = cbind(receiver_longitude, receiver_latitude), 
+            p2 = cbind(transmitter_longitude, transmitter_latitude)))]
+  
+  
+  
+  return(dtc2)
+}
